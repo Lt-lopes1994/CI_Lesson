fa718a1e4f149ff65dde56837ddaa7c04549f91b
Bruno Lopes <brunomantovanlopes@gmail.com>
2026-02-11 13:41:15 -0300

    feat: trigger CI

--- math.test.js
+++ math.test.js
@@ -1,6 +1,6 @@
 import teste from "node:test";
 import assert from "node:assert/strict";
-import { sum } from "./math.js";
+import { sum, multiply } from "./math.js";
 
 teste("sum of 1 and 2 should be 3", () => {
   assert.strictEqual(sum(1, 2), 3);
@@ -9,4 +9,8 @@ teste("sum of 1 and 2 should be 3", () => {
 teste("async test", async () => {
   const result = await Promise.resolve(42);
   assert.strictEqual(result, 42);
+});
+
+teste("mutiplication of 3 and 4 should be 12", () => {
+  assert.strictEqual(multiply(3, 4), 12);
 });
